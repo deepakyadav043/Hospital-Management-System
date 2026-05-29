b8ade3a625260c897834228cce12b7a97126d055
deepakyadav043 <deepakkssm2020@gmail.com>
2026-05-29 16:20:09 +0530

    first commit

--- hospital__app34.py
+++ hospital__app34.py
@@ -505,6 +505,52 @@ def show_login_page():
     """, unsafe_allow_html=True)
     _, col, _ = st.columns([1,2,1])
     with col:
+        st.markdown("""
+        <div style="
+        background:rgba(15,23,42,0.9);
+        border:1px solid rgba(20,184,166,0.3);
+        border-radius:16px;
+        padding:18px;
+        margin-bottom:20px;
+        color:white;
+        ">
+
+        <h3 style="color:#99f6e4;margin-top:0;">
+        🔐 Demo Login Credentials
+        </h3>
+
+        <table style="width:100%;color:white;">
+        <tr>
+        <th align="left">Role</th>
+        <th align="left">Username</th>
+        <th align="left">Password</th>
+        </tr>
+
+        <tr>
+        <td>🛡️ Admin</td>
+        <td>admin</td>
+        <td>admin@234</td>
+        </tr>
+
+        <tr>
+        <td>👨‍⚕️ Doctor</td>
+        <td>doctor</td>
+        <td>doctor@459</td>
+        </tr>
+
+        <tr>
+        <td>🗂️ Receptionist</td>
+        <td>reception</td>
+        <td>recep@389</td>
+        </tr>
+
+        </table>
+
+        </div>
+        """, unsafe_allow_html=True)
+
+
+
         role_label = st.selectbox("Login as", ["Admin","Doctor","Receptionist"])
         username   = st.text_input("Username", placeholder="Enter username")
         password   = st.text_input("Password", type="password", placeholder="Enter password")
@@ -541,49 +587,6 @@ def show_landing():
         if st.button("🔐 Staff Login", type="primary", use_container_width=True):
             st.session_state.show_login = True; st.rerun()
 
-    st.markdown("""
-    <div style="
-    background:rgba(15,23,42,0.9);
-    border:1px solid rgba(20,184,166,0.3);
-    border-radius:18px;
-    padding:20px;
-    margin-top:15px;
-    margin-bottom:20px;
-    color:white;
-    box-shadow:0 10px 30px rgba(0,0,0,0.3);
-    ">
-
-    <h3 style="color:#99f6e4;">🔐 Demo Login Credentials</h3>
-
-    <table style="width:100%;color:white;">
-    <tr>
-    <th align="left">Role</th>
-    <th align="left">Username</th>
-    <th align="left">Password</th>
-    </tr>
-
-    <tr>
-    <td>🛡️ Admin</td>
-    <td>admin</td>
-    <td>admin@234</td>
-    </tr>
-
-    <tr>
-    <td>👨‍⚕️ Doctor</td>
-    <td>doctor</td>
-    <td>doctor@459</td>
-    </tr>
-
-    <tr>
-    <td>🗂️ Receptionist</td>
-    <td>reception</td>
-    <td>recep@389</td>
-    </tr>
-
-    </table>
-
-    </div>
-    """, unsafe_allow_html=True)
 
     st.markdown("""
     <div class="hero">
